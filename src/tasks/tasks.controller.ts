bd0f74beabdd52429a87ab5bc7146bb2f29d461b
Endurance, the Martian <afriviral@gmail.com>
2023-03-15 22:53:40 +0100

    added getTaskById method for task/:id route

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -24,5 +24,15 @@ export class TasksController {
     @Post()
     createTask(@Body() createTaskDto: CreateTaskDto): Task {
         return this.tasksService.createTask(createTaskDto);
+    }
+
+    /**
+     * @description Get a task by id
+     * @param id {string}
+     * @returns {Task}
+     */
+    @Get('/:id')
+    getTaskById(@Param('id') id: string): Task {
+        return this.tasksService.getTaskById(id);
     }
 }
