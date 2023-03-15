5ccdea9fbb2ddec999a27e28fca94a7a857d9aa0
Endurance, the Martian <afriviral@gmail.com>
2023-03-15 22:44:01 +0100

    setup/ dto, models and enum

---
+++ src/tasks/task.model.ts
@@ -0,0 +1,7 @@
+import { TaskStatus } from './task-status.enum';
+export interface Task {
+    id: string;
+    title: string;
+    description: string;
+    status: TaskStatus;
+}
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
+import { Task } from './task.model';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Controller('tasks')
 export class TasksController {
@@ -10,7 +12,17 @@ export class TasksController {
      * @returns {Task[]}
      */
     @Get()
-    getAllTasks() {
+    getAllTasks(): Task[] {
         return this.tasksService.getAllTasks();
+    }
+
+    /**
+     * @description Create a new task
+     * @param CreateTaskDto {title, description}
+     * @returns {Task}
+     */
+    @Post()
+    createTask(@Body() createTaskDto: CreateTaskDto): Task {
+        return this.tasksService.createTask(createTaskDto);
     }
 }
