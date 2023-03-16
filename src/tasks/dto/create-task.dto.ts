e930457d3d7294398399e182777c7876b6a577e8
Endurance, the Martian <afriviral@gmail.com>
2023-03-16 18:23:51 +0100

    added validation & exception to tasks endpoints

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,4 +1,10 @@
+import { IsNotEmpty, Length } from 'class-validator';
+
 export class CreateTaskDto {
+    @IsNotEmpty()
+    @Length(4, 20)
     title: string;
+    @IsNotEmpty()
+    @Length(4, 255)
     description: string;
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task } from './task.model';
 import { TaskStatus } from './task-status.enum';
 import { v4 as uuid } from 'uuid';
@@ -47,7 +47,12 @@ export class TasksService {
     }
 
     getTaskById(id: string): Task {
-        return this.tasks.find((task) => task.id === id);
+        const task = this.tasks.find((task) => task.id === id);
+        if (!task) {
+            throw new NotFoundException(`Task with ID "${id}" not found`);
+        } else {
+            return task;
+        }
     }
 
     updateTaskStatus(id: string, status: TaskStatus): Task {
