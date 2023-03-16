d625c0a4503ae1604e335df6e0da3641a89ab124
Endurance, the Martian <afriviral@gmail.com>
2023-03-16 20:46:36 +0100

    setup/ configuration modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TasksModule } from './tasks/tasks.module';
+import { getEnvPath } from './common/helper/env.helper';
+
+const envFilePath: string = getEnvPath(`${__dirname}/common/envs`);
 
 @Module({
-    imports: [TasksModule],
+    imports: [
+        ConfigModule.forRoot({ envFilePath, isGlobal: true }),
+        TasksModule,
+    ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
+    const app: NestExpressApplication = await NestFactory.create(AppModule);
+    const config: ConfigService = app.get(ConfigService);
+    const port: number = config.get<number>('PORT');
+
     app.useGlobalPipes(new ValidationPipe());
-    await app.listen(3000);
+    await app.listen(port, () => {
+        console.log(
+            `[Nest] Application running on ${config.get<string>('BASE_URL')}`,
+        );
+    });
 }
 bootstrap();
