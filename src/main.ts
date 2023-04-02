f59d82bd88c9e392e19f6eace4b8097c38618842
Endurance, the Martian <afriviral@gmail.com>
2023-04-02 12:51:22 +0100

    added/ swagger doc implemented

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { ConfigService } from '@nestjs/config';
 import { TransformInterceptor } from './transformers/transform.interceptors';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
     const logger = new Logger();
@@ -17,6 +18,18 @@ async function bootstrap() {
 
     app.useGlobalInterceptors(new TransformInterceptor());
     app.useGlobalPipes(new ValidationPipe());
+
+    // Swagger documentation
+    const swaggerConfig = new DocumentBuilder()
+        .setTitle('ProTask API')
+        .setDescription('ProTask is a lightweight RestAPI for task management')
+        .setVersion('1.0')
+        .addTag('task')
+        .build();
+
+    const document = SwaggerModule.createDocument(app, swaggerConfig);
+    SwaggerModule.setup('api', app, document);
+
     await app.listen(port, () => {
         logger.log(`Application running on ${config.get<string>('BASE_URL')}`);
     });
